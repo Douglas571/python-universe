951e21e57018cccc4f952e0d6354569e3c28ef26
Douglas Socorro <douglassocorro.work@gmail.com>
2022-03-08 21:49:04 -0400

    Implemented the delete funciton to db example

--- db/__main__.py
+++ db/__main__.py
@@ -3,11 +3,34 @@ import sqlite3
 def save_author(c, author):
   pass
 
-def delete_quote(c, filter, author):
-  pass
+def delete_quotes(c, filters={}):
+  query = 'delete from quotes'
 
-def update_quote(c, filter, author):
-  pass
+  if filters:
+    query += 'where '
+    for k in filters.keys():
+      query += f'{k}=:{k} '
+
+  res = c.execute(query, filters).fetchall()
+  return res
+
+def update_quote(c, new_data, filters):
+  query = 'update quotes set '
+  where = {}
+
+  for k in new_data.keys():
+    query += f'{k}=:{k} '
+
+  query += 'where '
+  for k, v in filters.items():
+    new_k = 'old_' + k
+    query += f'{k}=:{new_k} '
+    where[new_k] = v
+
+  arguments = new_data | where
+
+  res = c.execute(query, arguments)
+  return res
 
 def get_quotes(c, filters={}, order_by=[]):
   quotes = []
@@ -18,14 +41,6 @@ def get_quotes(c, filters={}, order_by=[]):
 
     for k in filters.keys():
       query += f'{k}=:{k} '
-
-    """
-    if 'author' in filters.keys():
-      query += "author=:author "
-
-    if 'year' in filters.keys():
-      query += "year=:year"
-    """
 
   if len(order_by) > 0:
     query += "order by "
@@ -63,15 +78,11 @@ def main():
   cur = con.cursor()
 
   with con:
-    quote = {
-      "author": "Darth Vader",
-      "content": "Ven, unete a mí, y dominaremos el imperio.",
-      "old_author": "author 2",
-      "year": 3800
-    }
 
-    res = con.execute('update quotes set content=:content, author=:author where author=:old_author', quote)
-    quotes = get_quotes(con, filters={'author':'author 1'}, order_by=['popularity'])
+    update_quote(con, {'content':'La fuerza, fuerte en tí es'}, {'author': 'Yoda'})
+    delete_quotes(con)
+    # quotes = get_quotes(con, filters={'author':'author 1'}, order_by=['popularity'])
+    quotes = get_quotes(con)
 
 
   print('Quotes')
