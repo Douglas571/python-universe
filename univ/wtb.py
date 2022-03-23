f45400457df3aaeb406294c9548a2ea2d5a71ad1
Douglas Socorro <douglassocorro.work@gmail.com>
2022-03-22 20:48:27 -0400

    Added a program to convert words to binary and ascii code

--- univ/wtb.py
+++ univ/wtb.py
@@ -1,15 +1,20 @@
 import sys
 
+def process(l):
+  s = l
+  s_code = ord(s)
+  s_bin = bin(s_code)  
+
+  return s, s_code, s_bin
+
 def main():
+  word = 'example'
   if len(sys.argv) > 1:
     word = sys.argv[1]
 
+  for l in word:
+    s, c, b = process(l)
+    print(f'"{s}" => {c:>3} => {b}')
+
 if __name__ == '__main__':
   main()
-
-  a = 'a'
-  help(chr)
-  a_code = chr(a)
-  help(int)
-  # a_bin = int(a_code, 2)
-  # print(f'"a" => {a_code} => {a_bin}')
