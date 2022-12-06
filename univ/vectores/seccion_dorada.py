fc96b7038ca2e29cc87ee90edd7c5ce075765ec0
Douglas571 <douglassocorro.work@gmail.com>
2022-12-05 20:00:01 -0400

    work in progress

--- univ/vectores/seccion_dorada.py
+++ univ/vectores/seccion_dorada.py
@@ -1,24 +1,33 @@
 import math
 
 fi = 1.618033988
-e = 0.5
+e = 0.3
 a = 0
 b = 20
 
-# ------------
+# ------------    
 
-a1 = a*(1 - fi) + b * fi
-a2 = a * fi + b*(1 - fi)
+def run_golder_search(a, b):
+    a1 = a*(1 - fi) + b * fi
+    a2 = a * fi + b*(1 - fi)
 
-def f(x):
-    return 5*math.pi*x - x**2
+    def f(x):
+        return 5*math.pi*x - x**2
 
-fa1 = f(a1)
-fa2 = f(a2)
+    fa1 = f(a1)
+    fa2 = f(a2)
 
-if fa1 > fa2:
-    a = a1
-else:
-    b = a2
+    if fa1 > fa2:
+        return [a1, b, a1, a2, fa1, fa2]
+    else:
+        return [a, a2, a1, a2, fa1, fa2]
 
-print(f'a={a}; b={b}; a1={a1}; a2={a2}')
+for i in range(15):
+    a, b, a1, a2, fa1, fa2 = run_golder_search(a, b)
+    print(f'iteración #{i}')
+    print(f' a={a};\n b={b};\n a1={a1};\n a2={a2}')   
+    print(f'f({a1})={fa1}')
+    print(f'f({a2})={fa2}')
+    print()
+
+    if math.fabs(a1 - a2) < e: break
